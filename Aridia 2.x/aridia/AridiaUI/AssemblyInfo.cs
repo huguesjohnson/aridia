a9ef5f22b5cb9333217c846715f8f3951fc51163
huguesjohnson <hugues@huguesjohnson.com>
2021-01-09 09:54:11 -0600

    Annual copyright update

--- Aridia 2.x/aridia/AridiaUI/AssemblyInfo.cs
+++ Aridia 2.x/aridia/AridiaUI/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 /*
 Aridia: Phantasy Star III ROM Editor
-Copyright (c) 2007-2018 Hugues Johnson
+Copyright (c) 2007-2021 Hugues Johnson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -20,9 +20,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("aridia")]
 [assembly: AssemblyDescription("Aridia application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://www.huguesjohnson.com/")]
+[assembly: AssemblyCompany("https://www.huguesjohnson.com/")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright (c) 2007-2018 Hugues Johnson")]
+[assembly: AssemblyCopyright("Copyright (c) 2007-2021 Hugues Johnson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ There are currently two branches for Aridia:
 
 -1.x: Built against .NET 1.1, targeted for Windows XP SP2 and 2000. This is frozen unless any major bugs are found.
 
--2.x: Built against .NET 4, targeted for Windows XP SP3 and higher. This is the active development branch, all new features will be against this version. 
+-2.x: Built against .NET 4, targeted for Windows XP SP3 and higher. This is the active development branch, all new features will be against this version. This works with Mono but the UI looks a little funky.
